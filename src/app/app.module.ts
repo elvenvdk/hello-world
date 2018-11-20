f6d4c8ebf6f458e067d51746fefcb7659c7e399b
Alvin.VanDeKuech <alvin.vandekuech@bjss.com>
2018-11-20 13:51:04 -0500

    Add class binding, property binding, attribute binding, bootstrap, style binding, event binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from "@angular/core";
 import { CoursesComponent } from "./courses.component";
 
 import { AppComponent } from "./app.component";
+import { CoursesService } from "./courses.service";
+import { AuthorsService } from "./authors.service";
+import { AuthorsComponent } from './authors/authors.component';
 
 @NgModule({
-  declarations: [AppComponent, CoursesComponent],
+  declarations: [AppComponent, CoursesComponent, AuthorsComponent],
   imports: [BrowserModule],
-  providers: [],
+  providers: [CoursesService, AuthorsService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,7 +1,40 @@
 import { Component } from "@angular/core";
+import { CoursesService } from "./courses.service";
 
 @Component({
   selector: "courses",
-  template: "<h2>Courses</h2>"
+  template: `
+    <h2>{{ title }}</h2>
+    <img [src]="imageUrl" />
+    <ul>
+      <li *ngFor="let course of courses">
+      {{ course }}
+      </li>
+    </ul>
+    <table>
+      <tr>
+        <td [attr.colspan]="colSpan"></td> 
+      </tr>
+    </table>
+    <div (click)="onDivClicked()">
+      <button (click)="onSave($event)" class="btn btn-primary" [class.active]="isActive">Save</button>
+    </div>
+    <button class="btn" [class.btn-primary]="isActive" [class.btn-danger]="!isActive">Enter</button>
+    `
 })
-export class CoursesComponent {}
+export class CoursesComponent {
+  title = "List of courses";
+  imageUrl = "http://picsum.photos/300/400";
+  courses;
+  colSpan = 2;
+  isActive=false;
+  onSave = ($event) => {
+    $event.stopPropagation();
+    console.log("button was clicked", $event);
+  }
+  onDivClicked = () => console.log("Div was clicked");
+
+  constructor(service: CoursesService) {
+    this.courses = service.getCourses();
+  }
+}
